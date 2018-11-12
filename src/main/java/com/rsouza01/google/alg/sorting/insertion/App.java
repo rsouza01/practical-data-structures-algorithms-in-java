6393129e91e307e9cf4a635b93dec4219e7fc998
Rodrigo Souza <rsouza01@gmail.com>
2018-11-12 00:10:10 -0200

    Insertion sort

---
+++ src/main/java/com/rsouza01/google/alg/sorting/insertion/App.java
@@ -0,0 +1,16 @@
+package com.rsouza01.google.alg.sorting.insertion;
+
+import  java.util.Arrays;
+
+public class App {
+	
+	public static void main (String[] args) {
+
+		int[] array = InsertionSort.sort(new int[] {12,45,23,78,34,8,93});
+
+		System.out.println(Arrays.toString(array));
+
+		
+	}
+
+}
