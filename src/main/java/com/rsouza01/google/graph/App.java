563559f8a9fc8e5745c26c079bc2c07c4829df11
Rodrigo Souza <rsouza01@gmail.com>
2018-11-18 18:11:52 -0200

    Graph added

---
+++ src/main/java/com/rsouza01/google/graph/App.java
@@ -0,0 +1,17 @@
+package com.rsouza01.google.graph;
+
+
+public class App {
+	
+	public static void main (String[] args) {
+
+		try {
+			
+
+		} catch (Exception e) {
+			//TODO: handle exception
+		}
+
+		
+	}
+}
---
+++ src/main/java/com/rsouza01/google/graph/Graph.java
@@ -0,0 +1,12 @@
+package com.rsouza01.google.graph;
+
+
+public class Graph {
+
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
+
